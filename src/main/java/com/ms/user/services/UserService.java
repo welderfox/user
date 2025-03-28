d3ac27ad861ec927daaa46e50c5d82b2f9df295e
welder <welderfox@hotmail.com>
2025-03-28 01:47:07 -0300

    #insert/publish

--- src/main/java/com/ms/user/services/UserService.java
+++ src/main/java/com/ms/user/services/UserService.java
@@ -4,21 +4,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 
 @Service
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer  userProducer;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, UserProducer userProducer){
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
+    //insere usuario na base e publica o mesmo na fila
     @Transactional
     public UserModel save(UserModel userModel)
     {
         //Implementar post na fila.
-        return userRepository.save(userModel);
+        userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
